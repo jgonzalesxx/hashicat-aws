242c3e548895a343e2b69d9a5845aafa4fa6c59d
Jessica Gonzales <jessicagonzales@deltek.com>
2023-10-17 21:44:18 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,1 +1,6 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/app/jessie-dltksesh1/registry/modules/private/jessie-dltksesh1/s3-bucket/aws"
+  version = "2.8.0"
 
+  bucket_prefix = "jessiegonzales"
+}
